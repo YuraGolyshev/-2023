aff1962e1d2af72ce62e0e588974f8ee6f59ea91
Yura Golyshev <yura.golyshev2021@bk.ru>
2023-04-05 20:11:07 +0300

    Изиенили на итеративный вывод первых n чисел Фибоначчи

--- main.cpp
+++ main.cpp
@@ -4,16 +4,28 @@ using namespace std;
 
 int fib(int n)
 {
-	if(n == 0) return 0;
-    	if(n == 1) return 0;
-	if(n == 2) return 1;
-    	return fib(n-1)+fib(n-2);
+	int a=0;    int b=1;    int sum;
+    	for(int i=1;i<=n;i++)
+    	{
+        	if(i == 1)
+            		cout << a << endl;
+        	if(i == 2)
+            		cout << b << endl;
+        	if(i>2)
+        	{
+        		sum = a+b;
+        		a = b;
+        		b = sum;
+        		cout << sum << endl;
+        	}
+    	}
+    	return 0;
 }
 
 
 int main()
 {
         cout << "Hello, World!" << endl;
-	cout << fib(10) << endl;
+	fib(10);
 }
 
