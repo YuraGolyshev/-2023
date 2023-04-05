88864be19028aeab93302102bccd97a127bc81ac
Yura Golyshev <yura.golyshev2021@bk.ru>
2023-04-05 19:58:02 +0300

    Добавили рекурсивный вывод n числа Фибоначчи начиная с 1,1

--- main.cpp
+++ main.cpp
@@ -2,7 +2,17 @@
 
 using namespace std;
 
+int fib(int n)
+{
+    if(n == 0) return 0;
+    if(n == 1 || n == 2) return 1;
+    return fib(n-1)+fib(n-2);
+}
+
+
 int main()
 {
         cout << "Hello, World!" << endl;
+	cout << fib(10) << endl;
 }
+
