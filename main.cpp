15c7f801ce262f78d0cac308578423c6ee66b611
Yura Golyshev <yura.golyshev2021@bk.ru>
2023-04-05 20:05:53 +0300

    Изменили начальные значения с 1,1 на 0,1

--- main.cpp
+++ main.cpp
@@ -4,9 +4,10 @@ using namespace std;
 
 int fib(int n)
 {
-    if(n == 0) return 0;
-    if(n == 1 || n == 2) return 1;
-    return fib(n-1)+fib(n-2);
+	if(n == 0) return 0;
+    	if(n == 1) return 0;
+	if(n == 2) return 1;
+    	return fib(n-1)+fib(n-2);
 }
 
 
